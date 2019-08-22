6fcedaba992d293a19b5fd4526f82594e5fd6b27
aayush <aayush9152@gmail.com>
2019-08-22 23:14:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = require("express");
+    app = express()
+    server = require("http").createServer(app)
+    io = require("socket-io").listen(server)
+
+server.listen(3000)
+
+app.length("/",function(req,res){
+    res.sendfile(__dirname + "/index.html")
+})
