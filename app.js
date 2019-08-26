9b0902af27238ef012f2a5fe2097cb2ed89d0418
aayush <aayush9152@gmail.com>
2019-08-26 17:15:23 +0530

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
     app = express()
     server = require("http").createServer(app)
     io = require("socket.io").listen(server)
+    nicknames = [];
 
 server.listen(3000)
 
@@ -11,4 +12,19 @@ app.get("/",function(req,res){
 
 io.sockets.on('connection',function(socket){
     
+    socket.on("new-user" , function(data,callback){
+        if(nicknames.indexOf(data) !== -1){
+            callback(false)
+        }
+        else{
+            callback(true);
+            socket.nickname = data;
+            nicknames.push(socket.nickname);
+            io.sockets.emit("username",nicknames);
+        }
+    })
+    
+    socket.on('send-message',function(data){
+        io.sockets.emit("new-message",data)
+    })
 })
